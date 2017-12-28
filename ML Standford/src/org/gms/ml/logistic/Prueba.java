d658fbf15f22b3b93b83180ad53b61153652adc4
gonzalo <gonzalo.moros.ds@gmail.com>
2017-12-29 00:07:02 +0100

    ML Standford -> lrCostFunction v.1

--- ML Standford/src/org/gms/ml/logistic/Prueba.java
+++ ML Standford/src/org/gms/ml/logistic/Prueba.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.function.DoubleBinaryOperator;
 
+import org.apache.commons.math3.linear.DefaultRealMatrixChangingVisitor;
+import org.apache.commons.math3.linear.DefaultRealMatrixPreservingVisitor;
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealMatrixFormat;
@@ -35,7 +37,7 @@ public class Prueba {
 		
 		int m = X.getRowDimension();
 		
-		RealVector theta_t = MatrixUtils.createRealVector(new double[]{-2.0,-1.0,1.0,2.0});
+		RealMatrix theta_t = MatrixUtils.createColumnRealMatrix(new double[]{-2.0,-1.0,1.0,2.0});
 		RealMatrix X_t = MatrixUtils.createRealMatrix(5, 4);
 		double[] ones = new double[5];
 		Arrays.fill(ones, 1.0);
@@ -43,17 +45,29 @@ public class Prueba {
 		Arrays.fill(data_t, 1.0);
 		Arrays.parallelPrefix(data_t, (left, rigth) -> {return left + rigth;});
 		
-		X_t.setColumn(0, ones);
 		X_t.setSubMatrix(reshape(data_t, 5, 3).getData(), 0, 1);
+		X_t = X_t.scalarMultiply(1.0/10.0);
+		X_t.setColumn(0, ones);
+		RealMatrix y_t = MatrixUtils.createColumnRealMatrix(new double[] {1,0,1,0,1});
+		double lambda_t = 3;
+		print(X_t);
+		print(y_t);
 		
-		System.out.println(RealVectorFormat.getInstance().format(theta_t));
-		System.out.println(MatrixUtils.OCTAVE_FORMAT.format(X_t));
-		
-//		System.out.printf("dimensions X: %d x %d%n", X.getRowDimension(), X.getColumnDimension());
-//		System.out.printf("dimensions y: %d x %d%n", y.getRowDimension(), y.getColumnDimension());
-		//System.out.printf("dimensions y: %d x %d%n", theta_t.getDimension(), y.getColumnDimension());
+		Object[] cost_and_grad = lrCostFunction(theta_t, X_t, y_t, lambda_t);
+		fprintf("\nTesting lrCostFunction() with regularization");
+		fprintf("\nCost: %f\n", cost_and_grad[0]);
+		fprintf("Expected cost: 2.534819\n");
+		fprintf("Gradients:\n");
+		fprintf(" %f \n", (RealMatrix)cost_and_grad[1]);
+		fprintf("Expected gradients:\n");
+		fprintf(" 0.146561\n -0.548558\n 0.724722\n 1.398003\n");
 	}
 
+	private static void fprintf(String string, Object ... object) {
+		System.out.printf(string, object);
+		
+	}
+	
 	public static RealMatrix reshape(double[] data, int rows, int cols){
 		RealMatrix matrix = MatrixUtils.createRealMatrix(rows, cols);
 		int ixdata = 0;
@@ -63,5 +77,99 @@ public class Prueba {
 			}
 		}
 		return matrix;
+	}
+	
+	public static Object[] lrCostFunction(RealMatrix theta, RealMatrix X, RealMatrix y, double lambda) {
+		
+		Object[] result = new Object[2];
+		int m = y.getRowDimension();
+		double J = 0;
+		RealMatrix grad = MatrixUtils.createRealMatrix(theta.getRowDimension(),theta.getColumnDimension());
+		
+		RealMatrix Theta_X = X.multiply(theta);
+		RealMatrix y_negative = y.scalarMultiply(-1);
+		RealMatrix one_minus_y = y.scalarMultiply(-1).scalarAdd(1);
+		RealMatrix one_minus_sigmoid = sigmoid(Theta_X).scalarMultiply(-1).scalarAdd(1);
+		RealMatrix first_part = y_negative.transpose().multiply(log(sigmoid(Theta_X)));
+		RealMatrix second_part = one_minus_y.transpose().multiply(log(one_minus_sigmoid));
+		double sum_regu = lambda / (2 * m) * sum(power(theta.getSubMatrix(1, theta.getRowDimension()-1, 0, theta.getColumnDimension()-1)));
+		
+		J = (1.0 / m) * sum(first_part.subtract(second_part)) + sum_regu;
+		result[0] = J;
+		
+		RealMatrix error = MatrixUtils.createRealMatrix(Theta_X.getRowDimension(), Theta_X.getColumnDimension());
+		error.setRowMatrix(0, sigmoid(Theta_X.getSubMatrix(0, 0, 0, 0)).subtract(y.getSubMatrix(0, 0, 0, 0)));
+		error.setSubMatrix(sigmoid(Theta_X.getSubMatrix(1, m-1, 0, 0)).subtract(y.getSubMatrix(1, m-1, 0, 0)).getData(), 1, 0);
+		
+		grad.setRowMatrix(0, X.transpose().getSubMatrix(0, 0, 0, X.getRowDimension()-1).multiply(error).scalarMultiply(1.0/m));
+		RealMatrix grad_part = X.transpose().getSubMatrix(1, X.getColumnDimension()-1, 0, X.getRowDimension()-1).multiply(error).scalarMultiply(1.0/m);
+		RealMatrix regu_part = theta.getSubMatrix(1, theta.getRowDimension()-1, 0, 0).scalarMultiply(lambda/m);
+		grad.setSubMatrix(grad_part.add(regu_part).getData(),1,0);
+		result[1] = grad;
+		return result;
+	}
+		
+	public static RealMatrix sigmoid(RealMatrix Z) {
+		RealMatrix result = Z.copy();
+		result.walkInRowOrder(new DefaultRealMatrixChangingVisitor() {
+			@Override
+			public double visit(int row, int column, double value) {
+				return 1.0 / (1.0 + Math.exp(-value));
+			}
+		});
+		return result;
+	}
+	
+	public static RealMatrix log(RealMatrix Z) {
+		RealMatrix result = Z.copy();
+		result.walkInRowOrder(new DefaultRealMatrixChangingVisitor() {
+			@Override
+			public double visit(int row, int column, double value) {
+				return Math.log(value);
+			}
+		});
+		return result;
+	}
+	
+	public static double sum(RealMatrix matrix) {
+		double result = 0;
+		result = matrix.walkInRowOrder(new DefaultRealMatrixPreservingVisitor() {
+			double sum = 0.0;
+			@Override
+			public void visit(int row, int column, double value) {
+				sum += value;
+			}
+			@Override
+			public double end() {
+				return sum;
+			}
+		});
+		return result;
+	}
+	
+	public static RealMatrix power(RealMatrix Z) {
+		RealMatrix result = Z.copy();
+		result.walkInRowOrder(new DefaultRealMatrixChangingVisitor() {
+			@Override
+			public double visit(int row, int column, double value) {
+				return Math.pow(value, 2);
+			}
+		});
+		return result;
+	}
+	
+	public static void print(RealMatrix matrix) {
+		RealMatrixFormat formater = new RealMatrixFormat("\n", "\n", "\t", "", "\n", "   ");
+		System.out.println(formater.format(matrix));
+		//System.out.println(MatrixUtils.OCTAVE_FORMAT.format(matrix));
+	}
+	
+	private static void fprintf(String string, RealMatrix matrix) {
+		double[][] data = matrix.getData();
+		for(double[] rows:data) {
+			for(double col:rows) {
+				System.out.printf(string, col);
+			}
+		}
 	}
 }
